bb797f101939c3905faf2e8b7df6c84ef2cda2fa
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-11-27 12:08:23 +0530

    Added number and password validation in regex

---
+++ applyregex/PhoneAndPasswordValidation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace basics.applyregex
+{
+    internal class PhoneAndPasswordValidation
+    {
+        static void Main()
+        {
+ 
+            Console.Write("Enter your phone number: ");
+            string phone = Console.ReadLine();
+
+            if (IsValidPhone(phone))
+                Console.WriteLine("Valid Phone Number ✔");
+            else
+                Console.WriteLine("Invalid Phone Number ✘");
+
+            Console.WriteLine();
+
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            if (IsValidPassword(password))
+                Console.WriteLine("Valid Password ✔");
+            else
+                Console.WriteLine("Invalid Password ✘");
+        }
+
+        public static bool IsValidPhone(string phone)
+        {
+            if (string.IsNullOrWhiteSpace(phone))
+                return false;
+
+            string pattern = @"^[6-9]\d{9}$";
+
+            return Regex.IsMatch(phone, pattern);
+        }
+
+        public static bool IsValidPassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                return false;
+
+            string pattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+
+            return Regex.IsMatch(password, pattern);
+        }
+    }
+}
+
