87c7e1a243bb34ac1569bf9c07c30fa6a51c3431
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-12-03 16:50:09 +0530

    Added a logger code to implement Multicast Delegates

---
+++ Delegates/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public delegate void LogHandler(string message);
+
+namespace basics.Delegates
+{
+    internal class Logger
+    {
+        public static void LogToConsole(string msg)
+       => Console.WriteLine("Console Log: " + msg);
+
+        public static void LogToFile(string msg)
+            => Console.WriteLine("File Log: " + msg);
+
+        public static void LogToEmail(string msg)
+            => Console.WriteLine("Email Log: " + msg);
+    }
+
+    class Execute
+    {
+        static void Main()
+        {
+            LogHandler log = Logger.LogToConsole;
+            log += Logger.LogToFile;
+            log += Logger.LogToEmail;
+
+            log("User logged in successfully!");
+        }
+    }
+}
