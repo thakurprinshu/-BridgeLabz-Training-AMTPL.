265f8f6b53f5c69446032387589f787ec1590041
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-12-04 15:22:41 +0530

    Added the code for file operations using streamreadwrite

---
+++ fileIO/UseStream.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace basics.fileIO
+{
+    internal class UseStream
+    {
+        static void Main()
+        {
+            string path = "notes.txt";
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("First line");
+                sw.WriteLine("Second line");
+                sw.WriteLine("Third line");
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                Console.WriteLine("--- Reading File ---");
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
